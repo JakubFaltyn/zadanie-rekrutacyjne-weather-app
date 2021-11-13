a659dcaefebc76149a501a19df41f6ace22d2055
JakubFaltyn <jakubfaltyn@gmail.com>
2021-11-13 06:05:49 +0100

    Bug fix and style fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Select from "./Components/Select";
-import Forecast from "./Components/Select";
 
+import React, { useState } from 'react';
 const api = {
-  appid: "6cb1772baa677bc26b4c8e8ad9699eb6",
-  base_url: "https://api.openweathermap.org/data/2.5/",
-};
+  appid: "a0726456a2ff1d5376b2809e2fc075f3",
+  base_url: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
+  const options = [{value: "London", label: "London"},{value: "München", label: "München"}]
+
+  const data = (city) => {
+    if (city !== undefined) {
+      fetch( `${api.base_url}forecast?q=${city}&units=metric&APPID=${api.appid}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  }
 
   return (
-    <div className="App">
       <main>
-        <Select setCity={setCity} city={city}/>
-        <Forecast city={city}/>
+        <div className="single-select">
+          <select
+          value={city}
+          onChange={e => {data(e.target.value); setCity(e.target.value)}}>
+          {options.map(o => (
+            <option key={o.value} value={o.value}>{o.label}</option>
+          ))}
+        </select>
+        </div>
+        {(typeof weather.list != "undefined") ? (
+        <div>
+        <div className="location-info">
+          <div className="location-info__location">{weather.city.name}</div>
+          <div className="location-info__date">
+            <h1>Temperature: </h1>
+            {weather.list[0].dt_txt}
+            </div>
+        </div>
+        <div className="weather-info">
+          <div className="weather-info__temp">
+            <h1>Temperature: </h1>
+            {Math.round(weather.list[0].main.temp)}°c
+          </div>
+          <div className="weather-info__humidity">
+          <h1>Humidity: </h1>
+            {Math.round(weather.list[0].main.humidity)} %
+          </div>
+        </div>
+      </div>
+        ) : ('')}
       </main>
-    </div>
   );
 }
 
